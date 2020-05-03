3653f899a23175984dbc6bdb5468ec154aee9d2c
Dmitriy Frolov <omgoole@ya.ru>
2020-05-03 12:34:46 +0500

    new tests

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -5,12 +5,21 @@ import genDiff from '../src/index.js';
 
 const pathResolve = (fileName) => path.resolve('./__tests__/__fixtures__/', fileName);
 
-describe.each(['plain', 'json', 'pretty'])('%s type', (diffType) => {
+describe.each(['plain', 'json', 'pretty'])('%s format', (diffType) => {
   const result = fs.readFileSync(pathResolve(`${diffType}Result.txt`), 'utf-8');
 
   test.each(['json', 'yml', 'ini'])('should be equal [.%s]', (extname) => {
     const beforePath = pathResolve(`before.${extname}`);
     const afterPath = pathResolve(`after.${extname}`);
     expect(genDiff(beforePath, afterPath, diffType)).toEqual(result);
+  });
+});
+
+describe('default format [pretty]', () => {
+  const result = fs.readFileSync(pathResolve('prettyResult.txt'), 'utf-8');
+  test.each(['json', 'yml', 'ini'])('should be equal [.%s]', (extname) => {
+    const beforePath = pathResolve(`before.${extname}`);
+    const afterPath = pathResolve(`after.${extname}`);
+    expect(genDiff(beforePath, afterPath)).toEqual(result);
   });
 });
